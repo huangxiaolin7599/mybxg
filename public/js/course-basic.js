6e7c3fc9cce50e99caa170f722025b601de9a896
zhangsan <759953901@qq.com>
2017-06-13 21:18:49 +0800

    完成课时所有功能

--- public/js/course-basic.js
+++ public/js/course-basic.js
@@ -3,7 +3,7 @@ define(['jquery','template','util'],function($,template,util){
     util.setMenu('/course/add');
     // 获取课程id
     var csId = util.qs('cs_id',location.search);
-    if(csId){
+ 
         // 编辑操作
         // 根据课程id查询课程的详细信息
         $.ajax({
@@ -16,9 +16,5 @@ define(['jquery','template','util'],function($,template,util){
                 $('#courseBasicInfo').html(html);
             }
         });
-    }else{
-        // 添加操作
-        var html = template('courseBasicTpl',{});
-        $('#courseBasicInfo').html(html);
-    }
+
 });
